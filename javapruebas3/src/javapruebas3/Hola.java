0a1138f5ee05468d1bc5a7764713f3a351e08a22
Alex <Alex@DESKTOP-SF7MT98>
2019-03-21 18:18:43 +0100

    Primera version

---
+++ javapruebas3/src/javapruebas3/Hola.java
@@ -0,0 +1,11 @@
+package javapruebas3;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola de javapruebas3");
+
+	}
+
+}
